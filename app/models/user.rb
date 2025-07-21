bee01e6f6bd88f147ef36cf10aec5ff234dbbf4d
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-07-21 13:08:22 +0800

    feat: Create Post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts
+
   validates :username, length: { minimum: 5, maximum: 16 },
                       presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
